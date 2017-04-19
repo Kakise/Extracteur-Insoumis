fbcf59890b27a5db97e7cccf08046ae1477e7365
Labo Member 004 <me@kakise.xyz>
2017-04-19 19:59:18 +0200

    Update main.js

--- main.js
+++ main.js
@@ -47,7 +47,7 @@ app.post('/trimVideo', function (req, res) {
 
     stream.on('finish', () => {
         console.log('Download Complete ');
-        res.writeHead(200, {
+        res.send(200, {
             "Content-Type": "video/mp4",
             "Content-disposition": "filename='video-finale.mp4'"
         });
